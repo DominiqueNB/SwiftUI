4dac77fbf29c8742331ac0bb39491485451d2196
Dominique <dominique.bezerra@ifood.com.br>
2023-06-21 13:49:42 -0300

    App gesture and scale for image

--- Pinch App/Students/Pinch/Pinch/Screen/ContentView.swift
+++ Pinch App/Students/Pinch/Pinch/Screen/ContentView.swift
@@ -2,20 +2,82 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    private var image: some View = {
+        return Image("magazine-front-cover")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .cornerRadius(10)
+            .padding()
+            .shadow(color: .black.opacity(0.2), radius: 2, x: 2, y: 2)
+    }()
     
     // MARK: - FUNCTION
-    
+    func handleImageScale() {
+        if imageScale == 1 {
+            withAnimation(.spring()) {
+                imageScale = 5
+            }
+        } else {
+            resetImageState()
+        }
+    }
+
+    func resetImageState() {
+        withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+
+    private func dragGesture() -> some Gesture
+    {
+        DragGesture()
+            .onChanged({ value in
+                withAnimation(.linear(duration: 1)) {
+                    imageOffset = value.translation
+                }
+            })
+            .onEnded { _ in
+                if imageScale <= 1 {
+                    resetImageState()
+                }
+            }
+    }
     // MARK: - CONTENT
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                image
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        handleImageScale()
+                    }
+                    .gesture(dragGesture())
+            } //: ZStack
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+        } //:  NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
             .previewDevice("iPhone 13")
     }
 }
